ef1ba3f4ee820df51cc057e69151aa4367bcb382
Parthiv Ganguly <parthiv2048@gmail.com>
2026-03-18 10:59:10 -0400

    Initial Commit

---
+++ FavoriteLandmarks/FavoriteLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteLandmarksApp.swift
+//  FavoriteLandmarks
+//
+//  Created by Parthiv Ganguly on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
